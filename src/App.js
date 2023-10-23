7c5ab1aaa4d06a0216feb3ab68ab391c178077a2
Illia Dudchyk <illia.dudchyk.pz.2021@lpnu.ua>
2023-10-23 15:07:07 +0300

    working buttons

--- src/App.js
+++ src/App.js
@@ -5,10 +5,25 @@ import NewtonFractal from './NewtonFractal';
 
 function App() {
   return (
+<<<<<<< HEAD
     <div className="App">
      {/* <MyCanvas/> */}
      <NewtonFractal/>
     </div>
+=======
+   <>
+    
+    {/* <Menucontent/> */}
+    <Routes>
+    <Route path="/" element={<Navigate to="/home" />} />
+      <Route path = "/home" element = {<MainPage/>}/>
+      <Route path = "/home1" element = {<MainPage1/>}/>
+      <Route path = "/fractal1" element = {<FractalPage1/>}/>
+      <Route path = "/fractal2" element = {<FractalPage2/>}/>
+      <Route path = "*" element = {<BackMenu/>}/>
+    </Routes>
+    </>
+>>>>>>> 1026211 (working buttons)
   );
 }
 
--- src/FractalPage1.js
+++ src/FractalPage1.js
@@ -16,8 +16,23 @@ export function FractalPage1() {
     <div>
       <BackMenu text="Фрактали" />
       <div className='slider-canvas-container'>
+<<<<<<< HEAD
         <Slider onDepthChange={handleDepthChange} />
         <CanvasVichek depth={depth} className="canvasVicsek" />
+=======
+        <div onClick={() => navigate('/home1')} className='images-above'>
+          <img className='book-image right' src={BackImage} alt="Back" />
+          <img className='book-image left' src={QuestionImage} alt="Question" />
+        </div>
+        <Slider depth={depth} onDepthChange={handleDepthChange} />
+        <div className='canvasVicsek'>
+          <CanvasVichek depth={depth} />
+        </div>
+        <div className='button-container'>
+          <Button text="Vicsek" className= "active-vichek-button" />
+          <Button link = "/fractal2" text="Newton" className= "newton-button" />
+        </div>
+>>>>>>> 1026211 (working buttons)
       </div>
     </div>
   );
